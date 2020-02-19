cb773e852e819293949bd414c2d1d73f443026c7
yan.almeida@corp.globo.com <yan.almeida@corp.globo.com>
2020-02-19 20:40:39 -0300

    setup.py

--- gibbs_ffbs_mld_normal/setup.py
+++ gibbs_ffbs_mld_normal/setup.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from setuptools import setup, find_packages
 
 
@@ -5,24 +7,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="yanwerneck",
+    name="tcc",
     version="0.0.1",
     author="Yan Werneck",
     author_email="yan_werneck@yahoo.com.br",
     description="Dinamic Linear Model With Normal Distribution",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yanwerneck/tcc",
+    license = 'MIT',
     install_requires=[
         "numpy==1.18.1",
         "pandas==1.0.1",
         "scipy==1.4.1"
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+
     packages=find_packages(),
     python_requires='>=3.6',
 )
