8183894833180e3ae6d5437705fff7545724dfac
yan.almeida@corp.globo.com <yan.almeida@corp.globo.com>
2020-02-19 20:37:06 -0300

    setup.py

--- gibbs_ffbs_mld_normal/setup.py
+++ gibbs_ffbs_mld_normal/setup.py
@@ -1,4 +1,5 @@
-import setuptools
+from setuptools import setup, find_packages
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,11 +13,16 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yanwerneck/tcc",
-    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy==1.18.1",
+        "pandas==1.0.1",
+        "scipy==1.4.1"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=find_packages(),
     python_requires='>=3.6',
 )
